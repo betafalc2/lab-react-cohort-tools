f0c88b56bf76b2e38c703f88fd1f10b0dc239cfe
Betaf <beta.falc2@gmail.com>
2024-08-08 16:48:07 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -9,14 +10,15 @@ function App() {
 
   return (
     <div className="App relative z-20 pt-20">
-      <Navbar />
+      <Navbar/>
 
       <div className="pages">
-        <HomePage />
 
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
       </div>
 
     </div>
